e07cc8d612e4a6d7910d5e7d9fd0ad5a9efb8129
monicacholico <monicachol@hotmail.com>
2018-07-31 09:27:07 -0500

    Astronomers exercise

--- aliasing.rb
+++ aliasing.rb
@@ -1,1 +1,39 @@
-# this is a test
+class Counter
+  def initialize
+    @count = 0
+  end
+  def increment
+    @count +=1
+    puts @count
+  end
+end
+
+a = Counter.new
+b = Counter.new
+c = b
+d = c
+
+a.increment
+b.increment
+c.increment
+d.increment
+
+
+class CelestialBody
+  attr_accessor :type, :name
+end
+
+altair = CelestialBody.new
+altair.name = "Altair"
+altair.type = "star"
+polaris = altair
+polaris.name = "Polaris"
+vega = polaris
+vega.name = "Vega"
+
+puts altair.name, polaris.name, vega.name
+
+puts altair.object_id
+puts polaris.object_id
+puts vega.object_id
+
