4cb2a68d3dd45dfdb3b85204cbe996c7c1fcface
Monicacholico <monicachol@hotmail.com>
2018-09-07 19:48:43 -0500

    exercises with ruby

---
+++ Other/ruby_loops.rb
@@ -0,0 +1,31 @@
+# The following conde loops through a list of strings and each string
+
+# for loop
+
+list = ["I", "am", "a", "programmer", "!"]
+string = ""
+
+for item in list
+  string += item + " "
+end
+puts string
+
+# while loop
+
+n = 0
+ while n < 15
+   puts n
+   n = n + 1
+ end
+
+puts n
+
+# until loop. this basically does the same thing as the while loop
+
+
+n = 0
+
+until n == 15
+puts n
+  n = n + 1
+  end
